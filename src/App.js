faa43f62fe10ee49f37bef06adfbb81107213f84
Thomi Aditya Alhakiim <thomiaditya@gmail.com>
2021-09-02 16:31:08 +0700

    adding react-router-dom and creating config for router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./styles/app.scss";
+import Home from "./pages/Home.page";
+
+const routes = [
+	{
+		path: "/",
+		component: Home,
+	},
+];
 
 function App() {
 	return (
-		<div className="App">
-			<span>Art Review</span>
-		</div>
+		<Router>
+			<Switch>
+				{routes.map((route, i) => (
+					<Route exact path={route.path} render={route.component} key={i} />
+				))}
+			</Switch>
+		</Router>
 	);
 }
 
