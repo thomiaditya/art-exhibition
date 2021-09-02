04584d400856c95869746bf293cb62d5e927d126
Thomi Aditya Alhakiim <thomiaditya@gmail.com>
2021-09-02 15:59:55 +0700

    changing node-sass module to version 4.14.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import "./styles/app.scss";
 
 function App() {
-	console.log(process.env);
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+			<span>Art Review</span>
 		</div>
 	);
 }
