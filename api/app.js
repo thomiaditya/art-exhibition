6dbd56950779649161eac3c1d343bdeac568efd9
Thomi Aditya Alhakiim <thomiaditya@gmail.com>
2021-10-23 12:05:58 +0700

    preparing passport js

--- api/app.js
+++ api/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const passport = require("passport");
 
+/**
+ * Get express application object.
+ */
 const app = express();
 
+/**
+ * Configure this application that is using .env file.
+ */
 require("dotenv").config();
 
 /**
@@ -14,6 +20,16 @@ require("./config/database");
  * Automatically require all models from models directory.
  */
 require("./models");
+
+/**
+ * Import the configuration file and pass the local passport object through the config.
+ */
+require("./config/passport")(passport);
+
+/**
+ * Intialize passport in order to use it. Its like a turn on button.
+ */
+app.use(passport.initialize());
 
 /**
  * Using the express new implementation of bodyparser to parse request data in javascript object format.
@@ -30,7 +46,6 @@ app.use(require("./routes"));
  * Starting the server.
  */
 const PORT = process.env.API_PORT;
-
 app.listen(PORT, () => {
 	console.log(`Listening on http://localhost:${PORT}/ ...`);
 });
--- api/config/passport.js
+++ api/config/passport.js
@@ -1,9 +1,13 @@
+const mongoose = require("mongoose");
+const fs = require("fs");
+
 /**
  * Verify Method
- *
  * This module is needed to write the configuration needed on passport. Actually, there is only one method that needed the most
  * it is the verify method passport has.
  *
  * @param {passport} passport
  */
-module.exports = (passport) => {};
+module.exports = (passport) => {
+	// TODO Create passport configuration here.
+};
